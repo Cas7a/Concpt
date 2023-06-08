e9aa5103a422baf81882a49f243f1ef0735ff67c
Juan <juandas2425@gmail.com>
2023-06-07 21:58:09 -0500

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Nav/Navbar";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Navbar onShownCart={showCartHandler} />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
